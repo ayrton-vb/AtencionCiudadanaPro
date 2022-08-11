711f4b173fe96bdf092269f6eb1713a59f788a62
ayrton_trabajo <avbarja@gmail.com>
2022-08-11 09:41:11 -0400

    cache2

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -73,7 +73,10 @@ class ClienteController extends Controller
     public function index()
     {
         Artisan::call('config:cache');
+        Artisan::call('optimize');
+        Artisan::call('config:clear');
         Artisan::call('cache:clear');
+
 
         return view('cliente.index');
     }
