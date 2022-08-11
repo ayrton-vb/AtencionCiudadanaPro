8e9797034f6107bb9cb8fe25bf7f091634e935ca
ayrton_trabajo <avbarja@gmail.com>
2022-08-11 09:43:36 -0400

    cache3

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -72,8 +72,7 @@ class ClienteController extends Controller
 
     public function index()
     {
-        Artisan::call('config:cache');
-        Artisan::call('optimize');
+
         Artisan::call('config:clear');
         Artisan::call('cache:clear');
 
