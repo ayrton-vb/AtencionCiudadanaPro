cf7b4a2d24f5fe8c3486449e1e1e16e7fac14260
ayrton_trabajo <avbarja@gmail.com>
2023-05-30 12:08:35 -0400

    cambios reporte

--- resources/views/cliente/categorias.blade.php
+++ resources/views/cliente/categorias.blade.php
@@ -5,12 +5,47 @@
     <section class="mt-5 d-flex flex-column justify-content-center align-items-center pt-3  text-center w-50 m-auto" id="intro">
 
         <p data-aos="fade-down" class="pt-3  fs-2">
-            <span class="text-danger fw-bold">Trámites y servicios de {{$Area->alias}}</span>
+            <span class="text-danger fw-bold">Area {{$Area->alias}}</span>
         </p>
 
-            <a data-aos="fade-up" href="/clientes" type="button" class="btn btn-outline-danger  mx-2 fs-6 m-1  fw-bold mb-3 ">Volver</a>
+            <a data-aos="fade-up" href="/clientes" type="button" class="btn btn-outline-danger  mx-2 fs-6 m-1  fw-bold">Volver</a>
 
     </section>
+
+    <section class="d-flex flex-column justify-content-center align-items-center text-center w-50 m-auto" id="intro">
+        <p data-aos="fade-down" class="mt-4 fs-2">
+            <span class="text-danger fw-bold">Destacados</span>
+        </p>
+    </section>
+
+    <section>
+    <div class="container">
+            <div id="contenidoCategorias" class="row">
+      
+                          <div  class="col-lg-4 col-md-6 col-sm-12 mb-2">
+                              <div id="dentro" class="card" >
+                                  <div class="cardDestacado card-body">
+                                        <h5 class="card-title mt-0">Cronograma Mantenimieto de Luminarias</h5>
+                                      <div class="images-wrapperDestacado">
+                                      <a title="Los Tejos" href="/Archivos/CRONOGRAMA DE MANTENIMIENTO PRIMERA SEMANA MAYO (1).jpg" target="blank_"><img src="/Archivos/CRONOGRAMA DE MANTENIMIENTO PRIMERA SEMANA MAYO (1).jpg" alt="Cronograma" /></a>
+                                    
+                                      </div>
+                                   
+               
+                                  </div>
+                              </div>
+                          </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="d-flex flex-column justify-content-center align-items-center text-center w-50 m-auto" id="intro">
+        <p data-aos="fade-down" class="pt-3  fs-2">
+            <span class="text-danger fw-bold">Tramites y Servicios</span>
+        </p>
+    </section>
+
 
     <section >
         <div class="container">
@@ -37,10 +72,6 @@
 
             </div>
         </div>
-
-
-
-
     </section>
 
 
