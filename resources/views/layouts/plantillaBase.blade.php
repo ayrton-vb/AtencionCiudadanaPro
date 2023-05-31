bf8561fddad1c94a744522e98b8cb794987d5d11
ayrton_trabajo <avbarja@gmail.com>
2023-05-31 15:34:16 -0400

    dataTables

--- resources/views/layouts/plantillaBase.blade.php
+++ resources/views/layouts/plantillaBase.blade.php
@@ -211,6 +211,61 @@
 
 
 
+<script src="https://kit.fontawesome.com/655d53150e.js" crossorigin="anonymous"></script>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    
+    
+
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.bootstrap5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+    
+    <script>
+        $(document).ready(function() {
+        $('#table').DataTable({
+             "lengthMenu":[[50,150,-1],[50,150,300,"All"]],
+            responsive: "true",
+            dom: 'Bfrtilp',       
+              buttons:[ 
+                        {
+                          extend:    'excelHtml5',
+                          text:      '<i class="fas fa-file-excel"></i> ',
+                          titleAttr: 'Exportar a Excel',
+                          className: 'btn btn-success'
+                        },
+                        {
+                          extend:    'pdfHtml5',
+                          text:      '<i class="fas fa-file-pdf"></i> ',
+                          titleAttr: 'Exportar a PDF',
+                          className: 'btn btn-danger'
+                        },
+                        {
+                          extend:    'print',
+                          text:      '<i class="fa fa-print"></i> ',
+                          titleAttr: 'Imprimir',
+                          className: 'btn btn-info'
+                        },
+                      ]	      
+          });
+
+        } );
+    </script>
+
+
+
+
+
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
