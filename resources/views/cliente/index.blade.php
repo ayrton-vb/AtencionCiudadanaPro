e4f1faf09626d06a14dc3bcdc0a5595eac843a6c
Jhos <jhosmirjhonyqt@gmail.com>
2024-03-08 19:40:52 -0400

    Correccion de errores carrusel

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -463,14 +463,14 @@
       </div>
       <div class="modal-body">
         {{-- <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1"> --}}
-        <div id="carouselExample" class="carousel slide">
+        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-              <div class="carousel-item">
-                <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1">
-              </div>
-              <div class="carousel-item active">
-                <img class="img-fluid" src="{{asset('/imagenes/elalto_39.png')}}" alt="Imagen 1">
-              </div>
+                <div class="carousel-item active" data-bs-interval="2000">
+                    <img class="img-fluid" src="{{asset('/imagenes/elalto_39.png')}}" alt="Imagen 1">
+                  </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1">
+                </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
