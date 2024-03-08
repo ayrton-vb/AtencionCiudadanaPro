6f778ecf88f5783702d06a4f2efeb6b63941cc5a
Jhos <jhosmirjhonyqt@gmail.com>
2024-03-08 19:25:34 -0400

    Se agrego modal carrusel

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -458,11 +458,29 @@
   <div class="modal-dialog modal-mod">
     <div class="modal-content">
       <div class="modal-header">
-        CENSO 2024
+        Bienvenid@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1">
+        {{-- <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1"> --}}
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1">
+              </div>
+              <div class="carousel-item">
+                <img class="img-fluid" src="{{asset('/imagenes/elalto_39.png')}}" alt="Imagen 1">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Siguiente</span>
+            </button>
+          </div>
       </div>
     </div>
   </div>
