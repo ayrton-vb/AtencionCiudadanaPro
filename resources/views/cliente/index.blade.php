88cf93ea4f9239659292fe4d56e83f7eae527de7
Jhos <jhosmirjhonyqt@gmail.com>
2024-03-19 20:36:18 -0400

    Se agrego un segundo banner del censo

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -467,9 +467,12 @@
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="8000">
                     <img class="img-fluid" src="{{asset('/imagenes/elalto_39.png')}}" alt="Imagen 1">
-                  </div>
+                </div>
                 <div class="carousel-item" data-bs-interval="8000">
-                    <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 1">
+                    <img class="img-fluid" src="{{asset('/imagenes/censo_banner_2.jpg')}}" alt="Imagen 2">
+                </div>
+                <div class="carousel-item" data-bs-interval="8000">
+                    <img class="img-fluid" src="{{asset('/imagenes/banner_censo.png')}}" alt="Imagen 3">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
